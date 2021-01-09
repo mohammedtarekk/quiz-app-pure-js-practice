c616810137813ee5c5ced179676b8135a27f8485
mohammedtarekk <mohammedtarek74@yahoo.com>
2021-01-09 03:20:35 +0200

    Startup Template

---
+++ quiz_script.js
@@ -0,0 +1,4 @@
+document.getElementById("startButton").onclick = () => {
+    document.getElementById("startupWindow").classList.add("hide");
+    document.getElementById("quizWindow").classList.remove("hide");
+};
